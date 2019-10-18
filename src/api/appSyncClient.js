1fa8cbc0f51aae12103c73df316d11d2e065638e
Ali <ali@homerlearning.com>
2019-10-17 23:59:22 -0400

    Refactor AWSClient. Trigger graphql addCharge mutation

---
+++ src/api/appSyncClient.js
@@ -0,0 +1,16 @@
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import awsconfig from '../aws-exports';
+import Amplify, { API, graphqlOperation, Logger } from 'aws-amplify';
+Amplify.configure(awsconfig);
+
+
+const client = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.API_KEY, // or type: awsconfig.aws_appsync_authenticationType,
+    apiKey: awsconfig.aws_appsync_apiKey,
+  }
+});
+
+export default client;
