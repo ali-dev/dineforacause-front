39979e986a076d66f357303d849da8fd5e694f9f
Ali <ali@homerlearning.com>
2019-10-26 18:04:44 -0400

    Handle form fields onChange

--- src/forms/CreateEventForm.js
+++ src/forms/CreateEventForm.js
@@ -12,19 +12,20 @@ import { DateInput, TimeInput } from 'semantic-ui-calendar-react';
   ];
 
 
-// const handleChange = (event, {name, value}) => {
-//     if (this.state.hasOwnProperty(name)) {
-//       this.setState({ [name]: value });
-//     }
-//   }
+
 
 class CreateEventForm extends Component {
   constructor(props) {
     super(props);
- 
+ 	this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
+      eventName: '',
+      address: '',	
       date: '',
       time: '',
+      country: '',
+      state: '',
+      zipCode: ''      
     };
   }
 
@@ -32,6 +33,19 @@ class CreateEventForm extends Component {
     if (this.state.hasOwnProperty(name)) {
       this.setState({ [name]: value });
     }
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    let object = {};
+	data.forEach((value, key) => {object[key] = value});
+	let json = JSON.stringify(object);
+	console.log(json)
+	console.log(object)
+	
+    console.log(this.state);
+
   }
 
   componentDidMount() {
@@ -42,10 +56,10 @@ class CreateEventForm extends Component {
   	const { cause } = this.props;
     
     return (
-    	<Form>
+    	<Form onSubmit={this.handleSubmit}>
 	    <Form.Field required>
 	      <label>Event Name</label>
-	      <input />
+	      <input  id='eventName' name='eventName' placeholder='Event Name' />
 	    </Form.Field>
 	    
 	    <Form.Group widths='equal'>
@@ -68,25 +82,27 @@ class CreateEventForm extends Component {
 	    <div className="ui divider"></div>
 	    <Form.Field required>
 	      <label>Address</label>
-	      <input  />
+	      <input name="address" />
 	    </Form.Field>
 
-	    <Form.Field 
-	    	control={Select} 
+	    <Form.Select  
+	        onChange={this.handleChange}
 	    	options={countries}
 	    	label='Country'
+	    	name="country"
 	    />
 	    <Form.Group widths='equal'>
 	    
-	    <Form.Field 
-	    	control={Select} 
-	    	options={states}
-	    	label='State'
-	    	placeholder='State'
+	    <Form.Select 
+		    	options={states}
+		    	label='State'
+		    	placeholder='State'
+		    	name='state'
+		    	onChange={this.handleChange}
 	    />
 	    <Form.Field>
 	      <label>Zip Code</label>
-	      <input placeholder='Enter Zip Code' />
+	      <input id='zipCode' name='zipCode' placeholder='Enter Zip Code' />
 	    </Form.Field>
 	    
 	    </Form.Group>
@@ -96,70 +112,11 @@ class CreateEventForm extends Component {
 	    <Form.Field>
 	      <Checkbox label='I agree to the Terms and Conditions' />
 	    </Form.Field>
-	    <Button type='submit'>Submit</Button>
+	    <Button type='submit'  >Submit</Button>
 	  </Form>
   
     )
   }
 }
-
-// const CreateEventForm = () => (
-  
-// 	  <Form>
-// 	    <Form.Field required>
-// 	      <label>Event Name</label>
-// 	      <input />
-// 	    </Form.Field>
-	    
-// 	    <Form.Group widths='equal'>
-// 		    <DateInput
-// 	          name="date"
-// 	          placeholder="Date"
-// 	          iconPosition="left"
-// 	          value=""
-// 	        />
-// 	        <TimeInput
-// 	          name="time"
-// 	          placeholder="Time"
-// 	          iconPosition="left"
-// 	        />
-//         </Form.Group>
-        
-// 	    <div className="ui divider"></div>
-// 	    <Form.Field required>
-// 	      <label>Address</label>
-// 	      <input  />
-// 	    </Form.Field>
-
-// 	    <Form.Field 
-// 	    	control={Select} 
-// 	    	options={countries}
-// 	    	label='Country'
-// 	    />
-// 	    <Form.Group widths='equal'>
-	    
-// 	    <Form.Field 
-// 	    	control={Select} 
-// 	    	options={states}
-// 	    	label='State'
-// 	    	placeholder='State'
-// 	    />
-// 	    <Form.Field>
-// 	      <label>Zip Code</label>
-// 	      <input placeholder='Enter Zip Code' />
-// 	    </Form.Field>
-	    
-// 	    </Form.Group>
-	    
-// 	    <div className="ui divider"></div>
-	    
-// 	    <Form.Field>
-// 	      <Checkbox label='I agree to the Terms and Conditions' />
-// 	    </Form.Field>
-// 	    <Button type='submit'>Submit</Button>
-// 	  </Form>
-  
-  
-// )
 
 export default CreateEventForm
