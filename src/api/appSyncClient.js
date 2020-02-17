ae356f07a86cc038327ece188174563029e25467
Ali <ali@homerlearning.com>
2020-02-17 15:13:15 -0500

    Logging aws config

--- src/api/appSyncClient.js
+++ src/api/appSyncClient.js
@@ -3,7 +3,7 @@ import awsconfig from '../aws-exports';
 import Amplify from 'aws-amplify';
 Amplify.configure(awsconfig);
 
-
+console.log(awsconfig);
 const client = new AWSAppSyncClient({
   url: awsconfig.aws_appsync_graphqlEndpoint,
   region: awsconfig.aws_appsync_region,
