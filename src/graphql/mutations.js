70315f7c1cc0544266a4039c2b81a43adbc9c004
Ali <ali@homerlearning.com>
2020-02-19 13:50:31 -0500

    Adds dynamo db models

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -58,3 +58,120 @@ export const addEvent = `mutation AddEvent(
   }
 }
 `;
+export const createCause = `mutation CreateCause($input: CreateCauseInput!) {
+  createCause(input: $input) {
+    causeName
+    country
+    details
+    id
+    image
+    organizationId
+    organizationName
+  }
+}
+`;
+export const updateCause = `mutation UpdateCause($input: UpdateCauseInput!) {
+  updateCause(input: $input) {
+    causeName
+    country
+    details
+    id
+    image
+    organizationId
+    organizationName
+  }
+}
+`;
+export const deleteCause = `mutation DeleteCause($input: DeleteCauseInput!) {
+  deleteCause(input: $input) {
+    causeName
+    country
+    details
+    id
+    image
+    organizationId
+    organizationName
+  }
+}
+`;
+export const createEvent = `mutation CreateEvent($input: CreateEventInput!) {
+  createEvent(input: $input) {
+    cause
+    causeDetails
+    date
+    editId
+    eventName
+    hostEmail
+    hostName
+    id
+    location
+    maxCapacity
+    minDonation
+    organizationId
+    recommendedDonation
+    rsvpId
+    time
+    viewId
+  }
+}
+`;
+export const updateEvent = `mutation UpdateEvent($input: UpdateEventInput!) {
+  updateEvent(input: $input) {
+    cause
+    causeDetails
+    date
+    editId
+    eventName
+    hostEmail
+    hostName
+    id
+    location
+    maxCapacity
+    minDonation
+    organizationId
+    recommendedDonation
+    rsvpId
+    time
+    viewId
+  }
+}
+`;
+export const deleteEvent = `mutation DeleteEvent($input: DeleteEventInput!) {
+  deleteEvent(input: $input) {
+    cause
+    causeDetails
+    date
+    editId
+    eventName
+    hostEmail
+    hostName
+    id
+    location
+    maxCapacity
+    minDonation
+    organizationId
+    recommendedDonation
+    rsvpId
+    time
+    viewId
+  }
+}
+`;
+export const createOrganization = `mutation CreateOrganization($input: CreateOrganizationInput!) {
+  createOrganization(input: $input) {
+    name
+  }
+}
+`;
+export const updateOrganization = `mutation UpdateOrganization($input: UpdateOrganizationInput!) {
+  updateOrganization(input: $input) {
+    name
+  }
+}
+`;
+export const deleteOrganization = `mutation DeleteOrganization($input: DeleteOrganizationInput!) {
+  deleteOrganization(input: $input) {
+    name
+  }
+}
+`;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -75,3 +75,102 @@ export const getOrganizationInfo = `query GetOrganizationInfo($name: String!) {
   }
 }
 `;
+export const getCause = `query GetCause($id: ID!) {
+  getCause(id: $id) {
+    causeName
+    country
+    details
+    id
+    image
+    organizationId
+    organizationName
+  }
+}
+`;
+export const listCauses = `query ListCauses(
+  $filter: ModelCauseFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listCauses(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      causeName
+      country
+      details
+      id
+      image
+      organizationId
+      organizationName
+    }
+    nextToken
+  }
+}
+`;
+export const getEvent = `query GetEvent($id: ID!) {
+  getEvent(id: $id) {
+    cause
+    causeDetails
+    date
+    editId
+    eventName
+    hostEmail
+    hostName
+    id
+    location
+    maxCapacity
+    minDonation
+    organizationId
+    recommendedDonation
+    rsvpId
+    time
+    viewId
+  }
+}
+`;
+export const listEvents = `query ListEvents(
+  $filter: ModelEventFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      cause
+      causeDetails
+      date
+      editId
+      eventName
+      hostEmail
+      hostName
+      id
+      location
+      maxCapacity
+      minDonation
+      organizationId
+      recommendedDonation
+      rsvpId
+      time
+      viewId
+    }
+    nextToken
+  }
+}
+`;
+export const getOrganization = `query GetOrganization($id: ID!) {
+  getOrganization(id: $id) {
+    name
+  }
+}
+`;
+export const listOrganizations = `query ListOrganizations(
+  $filter: ModelOrganizationFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listOrganizations(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      name
+    }
+    nextToken
+  }
+}
+`;
