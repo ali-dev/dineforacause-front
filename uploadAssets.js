5cc6af0a90c05bc42396d40b28b5a894fb4c503a
Ali <ali@homerlearning.com>
2020-04-04 19:58:31 -0400

    Sets dynamic mime type

--- uploadAssets.js
+++ uploadAssets.js
@@ -1,17 +1,18 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 var fs = require('fs');
+var mime = require('mime-types');
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 // call S3 to retrieve upload file to specified bucket
-var uploadParams = {Bucket: 'cause-cuisine-site-assets', Key: '', Body: '', ACL: 'public-read', ContentType: 'text/css'};
+var uploadParams = {Bucket: 'cause-cuisine-site-assets', Key: '', Body: '', ACL: 'public-read'};
 
-const directories = ['build/assets/', 'build/static/css/', 'build/static/js/', 'build/static/media/']
+const directories = ['build/assets/', 'build/static/css/', 'build/static/js/', 'build/static/media/', 'build/']
 
-const additionalFiles = ['asset-manifest.json', 'favicon.ico', 'index.html', 'manifest.json', 'service-worker.js']
+// const additionalFiles = ['asset-manifest.json', 'favicon.ico', 'index.html', 'manifest.json', 'service-worker.js']
 // var files = fs.readdirSync('./build/');
 
 
@@ -24,10 +25,18 @@ for (let i = 0; i < directories.length; i++ ) {
     const directory = directories[i];
     const files = fs.readdirSync(__dirname+'/'+directory);
     for (let f = 0; f < files.length; f++) {
+            
         const file = files[f];
+        if (directory === 'build' && (file === 'static' || file === 'assets')) {
+            continue;
+        }
         const fullPath = directory+file;
         // Configure the file stream and obtain the upload parameters
         var fs = require('fs');
+        const mimetype = mime.lookup('json');
+        if (mimetype) {
+            uploadParams.ContentType = mimetype;
+        }    
         var fileStream = fs.createReadStream(__dirname+'/'+fullPath);
         fileStream.on('error', function(err) {
         console.log('File Error', err);
@@ -54,32 +63,6 @@ for (let i = 0; i < directories.length; i++ ) {
 
 
     }
-
-}
-
-const directory = 'build/';
-for (let f = 0; f < additionalFiles.length; f++) {
-    const file = additionalFiles[f];
-    const fullPath = directory+file;
-    // Configure the file stream and obtain the upload parameters
-    var fs = require('fs');
-    var fileStream = fs.createReadStream(__dirname+'/'+fullPath);
-    fileStream.on('error', function(err) {
-    console.log('File Error', err);
-    });
-    uploadParams.Body = fileStream;
-    var path = require('path');
-    uploadParams.Key = fullPath;
-    
-    // call S3 to retrieve upload file to specified bucket
-    s3.upload(uploadParams, function (err, data) {
-    if (err) {
-        console.log("Error", err);
-    } if (data) {
-        console.log("Upload Success", data.Location);
-    }
-    });
-
 
 }
 
