833165e4aa3925267cb43275b3593972c3bf1dfa
Ali <ali@homerlearning.com>
2020-05-23 00:38:22 -0400

    Removes old code

--- src/containers/AppWithCauses.js
+++ src/containers/AppWithCauses.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setSearchField, requestCauses } from '../actions';
-import './App.css';
+// import './App.css';
 import Header from '../components/Header'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
